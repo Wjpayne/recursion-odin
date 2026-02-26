98bbaaf17001cd4a113deda2be66ba904a3720ac
Wjpayne <payne.j.will@gmail.com>
2026-02-25 21:00:07 -0600

    finish fibs method using both iterative and recursion

--- fibs.js
+++ fibs.js
@@ -10,3 +10,17 @@ function fibs(n) {
   
   return sequence;
 }
+
+console.log(fibs(8)); 
+
+function fibsRec(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const sequence = fibsRec(n - 1);
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);   
+    return sequence;
+}
+
+console.log(fibsRec(8));
